9471e0c75fab1192347ff622f981bed383d9c607
Ronald Greeff <r.greeff@registry-trust.org.uk>
2019-08-14 15:29:51 +0100

    correct exercise

--- exercise.py
+++ exercise.py
@@ -1,89 +1,44 @@
-# THINGS WE'RE GOING TO USE
+"""
+We'll start with a list of the top 100 popular names. *Note that they are all capitalised.*
+"""
 
-print() # <- This is the print function. We'll get into functions later, you just need to know that it prints whatever you put into it, into the console.
-"CTRL + B" # <- You hit these two together to execute whatever you write in this file (a python script / .py file)
-
-print("""                 __,__\n        .--.  .-"     "-.  .--.\n       / .. \/  .-. .-.  \/ .. \\\n      | |  '|  /   Y   \  |'  | |\n      | \   \  \ 0 | 0 /  /   / |\n       \ '- ,\.-"`` ``"-./, -' /\n        `'-' /_   ^ ^   _\ '-'`\n        .--'|  \._ _ _./  |'--.\n      /`    \   \.-.  /   /    `\\\n     /       '._/  |-' _.'       \\\n    /          ;  /--~'   |       \\\n   /        .'\|.-\--.     \       \\\n  /   .'-. /.-.;\  |\|'~'-.|\       \\\n  \       `-./`|_\_/ `     `\'.      \\\n   '.      ;     ___)        '.`;    /\n     '-.,_ ;     ___)          \/   /\n      \   ``'------'\       \   `  /\n       '.    \       '.      |   ;/_\n     ___>     '.       \_ _ _/   ,  '--.\n   .'   '.   .-~~~~~-. /     |--'`~~-.  \\\n  // / .---'/  .-~~-._/ / / /---..__.'  /\n ((_(_/    /  /      (_(_(_(---.__    .'\n           | |     _              `~~`\n           | |     \'.\n            \ '....' |\n             '.,___.'\n""")
-
-
-# VARIABLES
-
-""" What is a variable?
-	Officially: Variables are used to store information to be referenced and manipulated in a computer program.
-	Think of it as a tag / label / container that stores some data. That data may change as you manipulate it,
-	but you can always get hold of the container again by calling its tag (the variable name) """
+top_100_names = ["OLIVIA", "RUBY", "EMILY", "GRACE", "JESSICA", "CHLOE", "SOPHIE", "LILY", "AMELIA", "EVIE", "MIA", "ELLA", "CHARLOTTE", "LUCY", "MEGAN", "ELLIE", "ISABELLE", "ISABELLA", "HANNAH", "KATIE", "AVA", "HOLLY", "SUMMER", "MILLIE", "DAISY", "PHOEBE", "FREYA", "ABIGAIL", "POPPY", "ERIN", "EMMA", "MOLLY", "IMOGEN", "AMY", "JASMINE", "ISLA", "SCARLETT", "LEAH", "SOPHIA", "ELIZABETH", "EVA", "BROOKE", "MATILDA", "CAITLIN", "KEIRA", "ALICE", "LOLA", "LILLY", "AMBER", "ISABEL", "LAUREN", "GEORGIA", "GRACIE", "ELEANOR", "BETHANY", "MADISON", "AMELIE", "ISOBEL", "PAIGE", "LACEY", "SIENNA", "LIBBY", "MAISIE", "ANNA", "REBECCA", "ROSIE", "TIA", "LAYLA", "MAYA", "NIAMH", "ZARA", "SARAH", "LEXI", "MADDISON", "ALISHA", "SOFIA", "SKYE", "NICOLE", "LEXIE", "FAITH", "MARTHA", "HARRIET", "ZOE", "EVE", "JULIA", "AIMEE", "HOLLIE", "LYDIA", "EVELYN", "ALEXANDRA", "MARIA", "FRANCESCA", "TILLY", "FLORENCE", "ALICIA", "ABBIE", "EMILIA", "COURTNEY", "MARYAM", "ESME"]
 
 
-# Without a variable
+# ---STEP 1----------------------------------------------------------------------------------------
+"""
+Let's write our first function. As a reminder, a function is structured like this:
 
-
-
-# print("I have 3 bananas.")
-# print("If I eat 1 banana, I now have 2 left.")
-
-
-# With a variable
-bananas = 3
-
-"I have {} bananas".format(bananas)
-
-# s1 = "I have {} bananas".format(bananas)
-
-number_of_bananas_eaten = 1
-bananas_remaining = bananas - number_of_bananas_eaten
-
-"If I eat {} banana, I now have {} left".format(number_of_bananas_eaten, bananas_remaining)
-
-# s2 = "If I eat {} banana, I now have {} left".format(number_of_bananas_eaten, bananas_remaining)
-
-bananas_per_banana_tree = 10
-banana_trees = 'a'
-
-
-# STRINGS
-
-string = "Hello, I am a string."
-
-
-# LISTS
-
-# DICTIONARIES
-
-# FUNCTIONS
-
-def my_function(input_a, input_b):
-	output = input_a + input_b
+def function_name(input_paramaters):
+	output = some_processing(input_parameters)
 	return output
 
-# FOR LOOPS
+you can name the function and the input_parameters whatever you want.
+"""
+
+# def my_function(my_name, list_of_names):
+# 	if my_name in list_of_names:
+# 		print(my_name + ' is in the list!')
+# 	else:
+# 		print(my_name + ' is not in the list')
 
 
+# ---STEP 2----------------------------------------------------------------------------------------
+"""
+Now let's create a variable to hold our name:
+"""
+
+# my_name = ''
 
 
-# EXERCISE
+# -------------------------------------------------------------------------------------------------
+# lower_cased_names = [name.title() for name in top_100_names]
 
-top_100_names = ["OLIVIA", "RUBY", "EMILY", "GRACE", "JESSICA", "CHLOE", "SOPHIE", "LILY", "AMELIA", "EVIE", "MIA", "ELLA",
-				"CHARLOTTE", "LUCY", "MEGAN", "ELLIE", "ISABELLE", "ISABELLA", "HANNAH", "KATIE", "AVA", "HOLLY", "SUMMER",
-				"MILLIE", "DAISY", "PHOEBE", "FREYA", "ABIGAIL", "POPPY", "ERIN", "EMMA", "MOLLY", "IMOGEN", "AMY", "JASMINE",
-				"ISLA", "SCARLETT", "LEAH", "SOPHIA", "ELIZABETH", "EVA", "BROOKE", "MATILDA", "CAITLIN", "KEIRA", "ALICE", "LOLA",
-				"LILLY", "AMBER", "ISABEL", "LAUREN", "GEORGIA", "GRACIE", "ELEANOR", "BETHANY", "MADISON", "AMELIE", "ISOBEL",
-				"PAIGE", "LACEY", "SIENNA", "LIBBY", "MAISIE", "ANNA", "REBECCA", "ROSIE", "TIA", "LAYLA", "MAYA", "NIAMH", "ZARA",
-				"SARAH", "LEXI", "MADDISON", "ALISHA", "SOFIA", "SKYE", "NICOLE", "LEXIE", "FAITH", "MARTHA", "HARRIET", "ZOE",
-				"EVE", "JULIA", "AIMEE", "HOLLIE", "LYDIA", "EVELYN", "ALEXANDRA", "MARIA", "FRANCESCA", "TILLY", "FLORENCE",
-				"ALICIA", "ABBIE", "EMILIA", "COURTNEY", "MARYAM", "ESME"]
 
-d = {}
-for name in top_100_names:
-	first = name[0]
+# ---STEP 3----------------------------------------------------------------------------------------
+"""
+Now let's see if our name is one of the popular ones.
+Let's call our function and pass in the required parameters
+"""
 
-	if not d.get(first):
-		d[first] = []
-
-	d[first].append(name)
-
-l = []
-for i in d:
-	l.append((i, len(d[i])))
-
-l.sort(key=lambda x: x[1], reverse=True)
-print(l)
+# my_function(my_name, lower_cased_names)
